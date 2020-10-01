2baffadc083fa658677cbf0e3f5938fedf233391
Santiago Aguilar Silva <48460335+saguilars@users.noreply.github.com>
2020-10-01 12:56:51 -0300

    modificacion ver y crear usuario
    1. Agregar estado para insertar usuario
    2. Agregar formato a fecha para visualizar usuario estudiante

--- src/controllers/controller.users.js
+++ src/controllers/controller.users.js
@@ -3,14 +3,14 @@ import { psql } from '../psqlAdapter';
 const userFunctions = {
 
     getAllUsers: () => {
-        const usersData = 'select id, firstname, lastname, username, password, status, createdat from public.user';
+        const usersData = "select id, firstname, lastname, username, password, status, TO_CHAR(createdat, 'DD/MM/YYYY') AS createdat  from public.user";
         return psql.manyOrNone(usersData);
     },
     createUser: async (params) => {
         try {
-            const { username, firstname, lastname, password } = params;
+            const { username, firstname, lastname, password, status } = params;
 
-            const query_insert = `INSERT INTO public.user (firstname, lastname, username, password) VALUES('${firstname}', '${lastname}','${username}', crypt('${password}',gen_salt('bf')))`;
+            const query_insert = `INSERT INTO public.user (firstname, lastname, username, password, status) VALUES('${firstname}', '${lastname}','${username}', crypt('${password}',gen_salt('bf')), '${status}')`;
             const resultado = await psql.any(query_insert)
                 .then((result) => {
                     console.log(params);
